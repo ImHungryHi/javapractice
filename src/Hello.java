824b21ac505d35b7ce09d95f797aec2df820ab3e
ChrisDS <chris.de.smedt.91@gmail.com>
2021-02-15 11:28:37 +0100

    My first commit on this place

---
+++ src/Hello.java
@@ -0,0 +1,37 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Twas brillig and the slithy toves\n" +
+                "did gyre and gimble in the wabe.\n" +
+                "All mimsy were the borogoves\n" +
+                "and the mome raths outgrabe.\n" +
+                "\n" +
+                "Beware the Jabberwock, my son.\n" +
+                "The jaws that bite, the claws that catch.\n" +
+                "Beware the Jubjub bird and shun\n" +
+                "the frumious Bandersnatch.\n" +
+                "\n" +
+                "He took his vorpal sword in hand.\n" +
+                "Long time the manxome foe he sought.\n" +
+                "So rested he by the Tumtum tree\n" +
+                "and stood awhile in thought.\n" +
+                "\n" +
+                "And as in uffish thought he stood;\n" +
+                "the Bandersnatch, with eyes of flame,\n" +
+                "came whiffling through the Tulgey wood\n" +
+                "and burbled as it came.\n" +
+                "\n" +
+                "One, two! One, two! And through! And Through!\n" +
+                "His vorpal blade went snicker-snack.\n" +
+                "He left it dead, and with its head\n" +
+                "he went galumphing back.\n" +
+                "\n" +
+                "And hast thou slain the Jabberwock?\n" +
+                "Come to my arms, my beamish boy.\n" +
+                "Oh frabjous day! Calloo, callay!\n" +
+                "He chortled in his joy.\n" +
+                "\n" +
+                "Twas brillig and the slithy toves\n" +
+                "did gyre and gimble in the wabe.\n" +
+                "All mimsy were the borogoves\nand the mome raths outgrabe.");
+    }
+}
