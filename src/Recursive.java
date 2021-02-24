05ac7d21339e69038e4ca6c635c44b1f0303f22a
ChrisDS <chris.de.smedt.91@gmail.com>
2021-02-24 16:54:16 +0100

    Added Euclids algorithm to the Recursive class

--- src/Recursive.java
+++ src/Recursive.java
@@ -79,4 +79,12 @@ public class Recursive {
         if (n == 2) return;
         bubbleSort(arr, n - 1);
     }
+
+    // Returns the greatest common divisor by Euclids algorithm
+    public static int gcdByEuclidsAlgorithm(int n1, int n2) {
+        if (n2 == 0) {
+            return n1;
+        }
+        return gcdByEuclidsAlgorithm(n2, n1 % n2);
+    }
 }
