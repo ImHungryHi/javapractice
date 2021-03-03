f41138107b2bfc3a2b2d6b23f76b63d0051be780
ChrisDS <chris.de.smedt.91@gmail.com>
2021-03-03 11:35:16 +0100

    Enum example added

---
+++ src/code/IntermediaryCode.java
@@ -0,0 +1,12 @@
+package code;
+
+public class IntermediaryCode {
+    // You can make enums, just like this
+    public static enum ImageTypes {
+        BMP,
+        JPG,
+        PNG
+    }
+
+    // Satisfy your C# delegates fetish with functional interfaces in Java
+}
