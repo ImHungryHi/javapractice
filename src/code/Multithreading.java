f5f3ab2499eb7d95a618e22e9ab68026d812011f
ChrisDS <chris.de.smedt.91@gmail.com>
2021-03-08 16:35:48 +0100

    Another singleton synchronisation, inside a method

--- src/code/Multithreading.java
+++ src/code/Multithreading.java
@@ -287,4 +287,20 @@ public class Multithreading {
             return president;
         }
     }
+
+    public static class IMF {
+        private static IMF imf;
+
+        public static IMF getFund() {
+            synchronized(IMF.class) {
+                if (imf == null) {
+                    imf = new IMF();
+                }
+            }
+            return imf;
+        }
+
+        private IMF() {
+        }
+    }
 }
