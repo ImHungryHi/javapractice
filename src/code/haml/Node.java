b9ae0c7984f182b9453e106300bf2a8c6c62e224
ChrisDS <chris.de.smedt.91@gmail.com>
2021-03-11 11:39:48 +0100

    Improved with inheritance

--- src/code/haml/Node.java
+++ src/code/haml/Node.java
@@ -1,30 +1,38 @@
 package code.haml;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
-public class Node {
+public class Node extends HashMap<String, String> {
     private Node parent;
-    private HashMap<String, String> attributes;
-
-    public Node() {
-        attributes = new HashMap<>();
-    }
+    //private HashMap<String, String> attributes;
 
     public Node(Node parent) {
+        //this.attributes = new HashMap<>();
+        super();
         this.parent = parent;
-        this.attributes = new HashMap<>();
     }
 
     public void addAttribute(String key, String value) {
-        attributes.put(key, value);
+        put(key, value);
     }
 
     public String getValue(String key) {
-        if (attributes.containsKey(key)) {
-            return attributes.get(key);
+        if (this.containsKey(key)) {
+            return this.get(key);
         }
 
         return null;
+    }
+
+    public ArrayList<String> getAttributeNames() {
+        ArrayList<String> result = new ArrayList<String>();
+
+        for (String key : keySet()) {
+            result.add(key);
+        }
+
+        return result;
     }
 
     public String getContent() {
