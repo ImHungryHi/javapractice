d8d4af6f86d6f28ff27ec1ef801d582e551be9e4
ChrisDS <chris.de.smedt.91@gmail.com>
2021-03-11 11:31:01 +0100

    Added explanation for inheritance vs composition

--- src/cardgame/SmartPile.java
+++ src/cardgame/SmartPile.java
@@ -2,7 +2,19 @@ package cardgame;
 
 import java.util.ArrayList;
 
+// example of expanding on existing classes by inheritance
 public class SmartPile extends ArrayList<Card> {
+    // In the case of inheritance vs composition, the above is inheritance (with or without expansion using methods)
+    //  the following fields are an example of composition. Ie adding another class's functionality as its own seperate part
+    //  in a class
+    //
+    //public Card first = this.get(0);
+    //public Card last = this.get(size() - 1);
+    //
+    // Don't do this btw, upon creation of a SmartPile instance, the arraylist is empty. Preferably, do this:
+    public Card first, last;
+    // Then initialize or update when an action is performed
+
     // add already exists, and so does isEmpty.
     public Card pop() {
         return remove(size() - 1);
