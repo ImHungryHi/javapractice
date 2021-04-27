4a7bae49ff8dfa5de77ce534cf1ec326997fd54c
ChrisDS <chris.de.smedt.91@gmail.com>
2021-04-27 09:53:10 +0200

    Added default and static methods in interface

--- src/code/interfacing/Shape.java
+++ src/code/interfacing/Shape.java
@@ -2,5 +2,11 @@ package code.interfacing;
 
 public interface Shape {
     float calcSurface();
-    float calcCircumference();
+    default float calcCircumference() {
+        throw new UnsupportedOperationException("Method not implemented in this shape.");
+    }
+
+    static void print() {
+        System.out.println("Cannot override static methods.");
+    }
 }
