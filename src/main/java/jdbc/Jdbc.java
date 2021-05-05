772aa805243c7b7972f045de4c9af38a79c47452
ChrisDS <chris.de.smedt.91@gmail.com>
2021-05-05 12:19:24 +0200

    New quote

--- src/main/java/jdbc/Jdbc.java
+++ src/main/java/jdbc/Jdbc.java
@@ -12,7 +12,7 @@ public class Jdbc {
 
         query = "INSERT INTO jordiquotes(author, quote) VALUES(?, ?)";
         //String[] params = { "Chris", "Unix is user friendly. It's just very particular about who its friends are." };
-        String[] params = { "Douglas Adams", "The answer to life, the universe and everything is... 42." };
+        String[] params = { "Marvin", "Life? Don't talk to me about life. Here I am, brain the size of a planet, and they tell me to take you up to the bridge. Call that job satisfaction? 'Cos I don't." };
         //insert(url, user, passwd, query, params);
 
         query = "UPDATE jordiquotes SET quote = ? WHERE ID = ?";
