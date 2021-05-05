6f4267894a99bd463eb9399c15337a45a8491301
ChrisDS <chris.de.smedt.91@gmail.com>
2021-05-05 10:47:51 +0200

    Updated select + segregated it

--- src/main/java/jdbc/Jdbc.java
+++ src/main/java/jdbc/Jdbc.java
@@ -6,22 +6,38 @@ public class Jdbc {
     public static void main(String[] args) throws SQLException {
         String url = "jdbc:mysql://ID109462_crud.db.webhosting.be:3306/ID109462_crud",
                 user = "ID109462_crud",
-                passwd = "code4croissants",
-                query = "SELECT * FROM jordiquotes";
+                passwd = "code4croissants";
+        select(url, user, passwd);
+    }
 
-        Connection conn = DriverManager.getConnection(url, user, passwd);
-        PreparedStatement stmt = conn.prepareStatement(query);
-        ResultSet result = stmt.executeQuery();
+    public static void select(String url, String user, String passwd) {
+        String query = "SELECT * FROM jordiquotes";
 
-        while (result.next()) {
-            // Column name should be given
-            System.out.println(result.getString("author") + " said \"" + result.getString("quote") + "\"");
+        // Use this type of structure if there are multiple pieces of code within each level of try-blocks
+        try (Connection conn = DriverManager.getConnection(url, user, passwd)) {
+            try (PreparedStatement stmt = conn.prepareStatement(query)) {
+                try (ResultSet result = stmt.executeQuery()) {
+
+                    while (result.next()) {
+                        // Column name should be given
+                        System.out.println(result.getString("author") + " said \"" + result.getString("quote") + "\"");
+                    }
+
+                    // In a try-catch statement, these would be executed on a fail if you put a try with resources
+                    /*result.close();
+                    stmt.close();
+                    conn.close();*/
+                }
+                catch (SQLException ex) {
+                    // Do some debug dumps
+                }
+            }
+            catch (SQLException ex) {
+                // Do some debug dumps
+            }
         }
-
-        // In a try-catch statement, these would be executed on a fail if you put a try with resources
-        result.close();
-        stmt.close();
-        conn.close();
-
+        catch (SQLException ex) {
+            // Do some debug dumps
+        }
     }
 }
