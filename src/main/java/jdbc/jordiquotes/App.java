b14689879f27dc6af84a2d18134b42863baa5955
ChrisDS <chris.de.smedt.91@gmail.com>
2021-05-07 15:09:41 +0200

    Separated getCows

--- src/main/java/jdbc/jordiquotes/App.java
+++ src/main/java/jdbc/jordiquotes/App.java
@@ -1,5 +1,6 @@
 package jdbc.jordiquotes;
 import com.github.ricksbrown.cowsay.Cowsay;
+import jdbc.jordiquotes.Config.*;
 import jdbc.jordiquotes.dao.QuoteDao;
 import jdbc.jordiquotes.daoimpl.QuoteDaoMySQL;
 import jdbc.jordiquotes.model.Quote;
@@ -25,7 +26,7 @@ public class App {
     }
 
     private static void generateCowQuote() {
-        ArrayList<String> cows = getCows();
+        ArrayList<String> cows = Config.getCows();
         Random random = new Random();
         QuoteDao quoteEngine = new QuoteDaoMySQL();
         Quote quote = quoteEngine.getRandomQuote();
@@ -53,57 +54,5 @@ public class App {
                 System.out.println("I only know how to respond to y or n...");
             }
         }
-    }
-
-    private static ArrayList<String> getCows() {
-        return new ArrayList<>() {{
-                    add("beavis");
-                    add("bong");
-                    add("bud-frogs");
-                    add("bunny");
-                    add("cheese");
-                    add("cower");
-                    add("daemon");
-                    add("default");
-                    add("dragon-and-cow");
-                    add("dragon");
-                    add("elephant-in-snake");
-                    add("eyes");
-                    add("flaming-sheep");
-                    add("ghostbusters");
-                    add("head-in");
-                    add("hellokitty");
-                    add("kiss");
-                    add("kitty");
-                    add("koala");
-                    add("kosh");
-                    add("luke-koala");
-                    add("mech-and-cow");
-                    add("meow");
-                    add("milk");
-                    add("moofasa");
-                    add("moose");
-                    add("mutilated");
-                    add("ren");
-                    add("satanic");
-                    add("sheep");
-                    add("skeleton");
-                    add("small");
-                    add("sodomized");
-                    add("squirrel");
-                    add("stegosaurus");
-                    add("stimpy");
-                    add("supermilker");
-                    add("surgery");
-                    add("telebears");
-                    add("three-eyes");
-                    add("turkey");
-                    add("turtle");
-                    add("tux");
-                    add("udder");
-                    add("vader-koala");
-                    add("vader");
-                    add("www");
-                }};
     }
 }
---
+++ src/main/java/jdbc/jordiquotes/Config.java
@@ -0,0 +1,57 @@
+package jdbc.jordiquotes;
+
+import java.util.ArrayList;
+
+public class Config {
+    public static ArrayList<String> getCows() {
+        return new ArrayList<>() {{
+            add("beavis");
+            add("bong");
+            add("bud-frogs");
+            add("bunny");
+            add("cheese");
+            add("cower");
+            add("daemon");
+            add("default");
+            add("dragon-and-cow");
+            add("dragon");
+            add("elephant-in-snake");
+            add("eyes");
+            add("flaming-sheep");
+            add("ghostbusters");
+            add("head-in");
+            add("hellokitty");
+            add("kiss");
+            add("kitty");
+            add("koala");
+            add("kosh");
+            add("luke-koala");
+            add("mech-and-cow");
+            add("meow");
+            add("milk");
+            add("moofasa");
+            add("moose");
+            add("mutilated");
+            add("ren");
+            add("satanic");
+            add("sheep");
+            add("skeleton");
+            add("small");
+            add("sodomized");
+            add("squirrel");
+            add("stegosaurus");
+            add("stimpy");
+            add("supermilker");
+            add("surgery");
+            add("telebears");
+            add("three-eyes");
+            add("turkey");
+            add("turtle");
+            add("tux");
+            add("udder");
+            add("vader-koala");
+            add("vader");
+            add("www");
+        }};
+    }
+}
