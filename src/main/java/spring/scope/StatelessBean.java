1ef044a8df01035055f25313f546cc8ac30f63bc
ChrisDS <chris.de.smedt.91@gmail.com>
2021-05-17 11:34:04 +0200

    Added lifecycle in spring

--- src/main/java/spring/scope/StatelessBean.java
+++ src/main/java/spring/scope/StatelessBean.java
@@ -1,4 +1,11 @@
 package spring.scope;
 
 public class StatelessBean {
+    public void init() {
+        System.out.println("Initialized");
+    }
+
+    public void destroy() {
+        System.out.println("Destroyed");
+    }
 }
