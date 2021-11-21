98e6b5bb0dbb0d52717cd78bfde4853c19b15cd9
ChrisDS <chris.de.smedt.91@gmail.com>
2021-11-21 19:32:24 +0100

    We start with a baseline

--- test/code/tdd/MathsTest.java
+++ test/code/tdd/MathsTest.java
@@ -1,6 +1,11 @@
 package code.tdd;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static code.tdd.Maths.*;
 
 class MathsTest {
     // Start off somewhere silly - like doing nothing
@@ -11,5 +16,12 @@ class MathsTest {
         // When
 
         // Then
+    }
+
+    // In reality, we would get rid of the doNothing function immediately after running and test all assertions in
+    //  this function
+    @Test
+    void shouldReturn_PrimeFactorList_V1() {
+        assertEquals(PrimeFactorsOf_V1(1), new ArrayList<>());
     }
 }
