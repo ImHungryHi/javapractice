33860cf39298b421d67ee059ac4fe5b67b29e38a
ChrisDS <chris.de.smedt.91@gmail.com>
2021-11-21 19:24:07 +0100

    Starting off tdd

---
+++ src/main/java/code/tdd/Maths.java
@@ -0,0 +1,9 @@
+package code.tdd;
+import java.util.*;
+
+// Prepare to be frustrated by the minutia of TDD...
+public class Maths {
+    public static List<Integer> PrimeFactorsOf_V1(int n) {
+        return new ArrayList<Integer>();
+    }
+}
---
+++ test/code/tdd/MathsTest.java
@@ -0,0 +1,15 @@
+package code.tdd;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathsTest {
+    // Start off somewhere silly - like doing nothing
+    @Test
+    void doNothing() {
+        // Given
+
+        // When
+
+        // Then
+    }
+}
