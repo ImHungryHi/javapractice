9de93c8da4bf3e99ebeecaba42a5d6c865e36e17
ChrisDS <chris.de.smedt.91@gmail.com>
2021-11-21 19:37:10 +0100

    Take the next step in test cases - add a 2

--- src/main/java/code/tdd/Maths.java
+++ src/main/java/code/tdd/Maths.java
@@ -6,4 +6,14 @@ public class Maths {
     public static List<Integer> PrimeFactorsOf_V1(int n) {
         return new ArrayList<Integer>();
     }
+
+    public static List<Integer> PrimeFactorsOf_V2(int n) {
+        List<Integer> result = new ArrayList<>();
+
+        if (n > 1) {
+            result.add(2);  // Ooooh, the frustration... But the test will pass and return empty for 1 and asList(2) for 2
+        }
+
+        return result;
+    }
 }
--- test/code/tdd/MathsTest.java
+++ test/code/tdd/MathsTest.java
@@ -24,4 +24,10 @@ class MathsTest {
     void shouldReturn_PrimeFactorList_V1() {
         assertEquals(PrimeFactorsOf_V1(1), new ArrayList<>());
     }
+
+    @Test
+    void shouldReturn_PrimeFactorList_V2() {
+        assertEquals(PrimeFactorsOf_V2(1), new ArrayList<>());
+        assertEquals(PrimeFactorsOf_V2(2), Arrays.asList(2));   // This would fail without changing the Maths class
+    }
 }
